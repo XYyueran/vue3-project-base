3204e20bb9969bbc99bc94541b8e39e34ca50e31
songjiahao <songjiahao@yofish.com>
2023-09-12 11:14:59 +0800

    feat:初始化和一些webpack 配置

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2>webpack5-vue3-ts</h2>
+  </template>
+  
+  <script setup lang="ts">
+    import './app.css'
+    import './app.less'
+    console.log('NODE_ENV', process.env.NODE_ENV)
+    console.log('BASE_ENV', process.env.BASE_ENV)
+  </script>
+  <style scoped>
+  </style>
+  
